60f60c7b38271c77719f94f86458998afd222ce0
Ritesh Das <riteshdas8652@gmail.com>
2025-06-11 20:52:05 +0530

    User controller & routes

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 
-
+const userRoute = require('./routes/user.route');
+const cookieParser = require('cookie-parser');
 
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
+
+app.use('/ss/v1/user',userRoute);
 
 module.exports = app;
---
+++ backend/routes/user.route.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const {register, login, logout, updateProfile} = require('../controllers/user.controller');
+const isAuthenticated = require('../middleware/authenticated');
+
+router.post('/register',register);
+router.post('/login',login);
+router.put('/profile/update',isAuthenticated,updateProfile);
+
+module.exports = router;
