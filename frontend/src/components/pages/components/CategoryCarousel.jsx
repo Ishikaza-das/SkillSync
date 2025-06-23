28ec8c9e5720084bd846a7b557ac53e21d7fe881
Ritesh Das <riteshdas8652@gmail.com>
2025-06-23 19:20:46 +0530

    Category Carousel

--- frontend/src/components/pages/components/CategoryCarousel.jsx
+++ frontend/src/components/pages/components/CategoryCarousel.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../../ui/carousel";
+import React from "react";
+
+const category = [
+  "Frontend Developer",
+  "Backend Developer",
+  "Full Stack Developer",
+  "Graphic Designer",
+  "Data Scientist",
+  "DevOps Engineer",
+  "Cybersecurity Specialist",
+  "Project Manager",
+  "Business Analyst",
+  "Sales Manager",
+  "HR Executive",
+  "Content Writer",
+  "SEO Specialist",
+  "Brand Strategist",
+  "Accountant",
+  "Financial Analyst",
+];
 
 const CategoryCarousel = () => {
   return (
     <div>
-      
+      <Carousel className="w-full max-w-xl mx-auto my-20">
+        <CarouselContent>
+          {category.map((cat, index) => (
+            <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+              <Button className="rounded-full" variant="outline">
+                {cat}
+              </Button>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
-  )
-}
+  );
+};
 
-export default CategoryCarousel
+export default CategoryCarousel;
