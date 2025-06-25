8a926ff89c0756570fd51071d0db5aad56ae47f1
Ritesh Das <riteshdas8652@gmail.com>
2025-06-25 14:46:29 +0530

    Applied Jobs

--- frontend/src/components/pages/components/AppliedJobTable.jsx
+++ frontend/src/components/pages/components/AppliedJobTable.jsx
@@ -1,11 +1,35 @@
+import { Badge } from '@/components/ui/badge';
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import React from 'react'
 
 const AppliedJobTable = () => {
   return (
     <div>
-      Applied Job Table
+      <Table>
+        <TableCaption>List of your Applied jobs</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Date</TableHead>
+            <TableHead>Job Role</TableHead>
+            <TableHead>Company</TableHead>
+            <TableHead className='text-right'>Status</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {
+            [1,2,3].map((item,index) => (
+              <TableRow key={index}>
+                <TableCell>25-06-2025</TableCell>
+                <TableCell>Frontend Developer</TableCell>
+                <TableCell>Google</TableCell>
+                <TableCell className='text-right'><Badge>Selected</Badge></TableCell>
+              </TableRow>
+            ))
+          }
+        </TableBody>
+      </Table>
     </div>
   )
 }
 
-export default AppliedJobTable
+export default AppliedJobTable;
