e649ed21f15d57d91debb0b4b4202dfdccac3756
Ritesh Das <riteshdas8652@gmail.com>
2025-06-26 12:06:03 +0530

    Profile Updated

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -6,7 +6,7 @@ const { singleUpload } = require('../middleware/multer');
 
 router.post('/register',singleUpload,register);
 router.post('/login',login);
-router.put('/profile/update',isAuthenticated,updateProfile);
+router.put('/profile/update',isAuthenticated,singleUpload,updateProfile);
 router.get('/logout',logout);
 
 module.exports = router;
