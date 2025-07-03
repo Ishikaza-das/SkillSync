cfb53f8a52fb62a8a2f1af05dc244a0659c63bc5
Ritesh Das <riteshdas8652@gmail.com>
2025-07-03 15:01:39 +0530

    New Company Update

--- backend/routes/company.route.js
+++ backend/routes/company.route.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { registerCompany, getCompany, getCompanyById, updateCompany } = require('../controllers/company.controller');
 const isAuthenticated = require('../middleware/authenticated');
+const { singleUpload } = require('../middleware/multer');
 
 router.post('/register',isAuthenticated,registerCompany);
 router.get('/get',isAuthenticated, getCompany);
 router.get('/get/:id',isAuthenticated, getCompanyById);
-router.put('/update/:id',isAuthenticated, updateCompany);
+router.put('/update/:id',isAuthenticated, singleUpload, updateCompany);
 
 module.exports = router;
--- frontend/src/components/pages/company/components/CompanySetup.jsx
+++ frontend/src/components/pages/company/components/CompanySetup.jsx
@@ -2,8 +2,11 @@ import Navbar from "@/components/shared/Navbar";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import axios from "axios";
 import { ArrowLeft } from "lucide-react";
 import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "sonner";
 
 const CompanySetup = () => {
   const [input, setInput] = useState({
@@ -14,6 +17,10 @@ const CompanySetup = () => {
     file: null,
   });
 
+  const [loading, setLoading] = useState(false);
+  const params = useParams();
+  const navigate = useNavigate()
+
   const chnageEventHandler = (e) => {
     setInput({ ...input, [e.target.name]: e.target.value });
   };
@@ -23,9 +30,41 @@ const CompanySetup = () => {
     setInput({ ...input, file });
   };
 
+  const back = () => {
+    navigate('/companies')
+  }
+
   const submitHandler = async (e) => {
     e.preventDefault();
-    console.log(input);
+    
+    const formData = new FormData();
+    formData.append('name',input.name);
+    formData.append('description',input.description);
+    formData.append('website',input.website);
+    formData.append('location',input.location);
+
+    if(input.file){
+      formData.append('file',input.file);
+    }
+
+    try {
+      setLoading(true);
+      const response = await axios.put(`${import.meta.env.VITE_COMPANY}/update/${params.id}`, formData, {
+        headers:{
+          'Content-Type':'multipart/form-data'
+        },
+        withCredentials:true
+      });
+      if(response.data.success){
+        toast.success(response.data.message);
+        navigate('/companies')
+      }
+    } catch (error) {
+      console.log(error);
+      // toast.error(error.response.data.message);
+    }finally{
+      setLoading(false);
+    }
   };
   return (
     <div>
@@ -37,6 +76,7 @@ const CompanySetup = () => {
             <Button
               variant="outline"
               className="flex items-center gap-2 text-gray-500 font-semibold"
+              onClick={back}
             >
               <ArrowLeft />
               <span>Back</span>
@@ -93,9 +133,20 @@ const CompanySetup = () => {
           </div>
           <div className="px-8 pb-8">
             {" "}
-            <Button type="submit" className="w-full" variant="purple2">
+            {loading ? (
+            <Button className="my-4 w-full h-10" variant="purple2">
+              {" "}
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please wait...
+            </Button>
+          ) : (
+            <Button
+              type="submit"
+              className=" w-full "
+              variant="purple2"
+            >
               Update
             </Button>
+          )}
           </div>
         </form>
       </div>
