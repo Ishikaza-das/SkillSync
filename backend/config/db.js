33259f01343bb81213d7e345a3dee968c7bce8c3
Ritesh Das <riteshdas8652@gmail.com>
2025-07-13 10:46:14 +0530

    DB Connect fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        const connect = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${connect.connection.host}`);
-    } catch (error) {
-        console.error(`MongoDB connection error: ${error.message}`);
-        process.exit(1);
-    }
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI, {
+      tls: true,
+    });
+    console.log(`MongoDB Connected: ${connect.connection.host}`);
+  } catch (error) {
+    console.error(`MongoDB connection error: ${error.message}`);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
