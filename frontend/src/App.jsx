40f2b67eb71739d501d7f6ac1d84dcef8dc272c1
Ritesh Das <riteshdas8652@gmail.com>
2025-07-16 15:24:49 +0530

    edit job fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,84 +1,113 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './App.css'
-import Login from './components/auth/Login'
-import Signup from './components/auth/Signup'
-import Home from './components/pages/Home'
-import Jobs from './components/pages/Jobs'
-import Browse from './components/pages/Browse'
-import Profile from './components/pages/Profile'
-import JobDescription from './components/pages/JobDescription'
-import Companies from './components/pages/company/Companies'
-import CompanyCreate from './components/pages/company/CompanyCreate'
-import CompanySetup from './components/pages/company/components/CompanySetup'
-import CompanyJobs from './components/pages/company/CompanyJobs'
-import PostJob from './components/pages/company/components/PostJob'
-import Applicants from './components/pages/company/Applicants'
-import CompanyProtectedRoute from './components/protected/CompanyProtectedRoute'
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
+import Home from "./components/pages/Home";
+import Jobs from "./components/pages/Jobs";
+import Browse from "./components/pages/Browse";
+import Profile from "./components/pages/Profile";
+import JobDescription from "./components/pages/JobDescription";
+import Companies from "./components/pages/company/Companies";
+import CompanyCreate from "./components/pages/company/CompanyCreate";
+import CompanySetup from "./components/pages/company/components/CompanySetup";
+import CompanyJobs from "./components/pages/company/CompanyJobs";
+import PostJob from "./components/pages/company/components/PostJob";
+import Applicants from "./components/pages/company/Applicants";
+import CompanyProtectedRoute from "./components/protected/CompanyProtectedRoute";
 
 const appRouter = createBrowserRouter([
   {
-    path:'/',
-    element:<Home/>
+    path: "/",
+    element: <Home />,
   },
   {
-    path:'/login',
-    element:<Login/>
+    path: "/login",
+    element: <Login />,
   },
   {
-    path:'/signup',
-    element:<Signup/>
+    path: "/signup",
+    element: <Signup />,
   },
   {
-    path:'/jobs',
-    element:<Jobs/>
+    path: "/jobs",
+    element: <Jobs />,
   },
   {
-    path:'/description/:id',
-    element:<JobDescription/>
+    path: "/description/:id",
+    element: <JobDescription />,
   },
   {
-    path:'/browse',
-    element:<Browse/>
+    path: "/browse",
+    element: <Browse />,
   },
   {
-    path:'/profile',
-    element:<Profile/>
+    path: "/profile",
+    element: <Profile />,
   },
   {
-    path:'/companies',
-    element:<CompanyProtectedRoute><Companies/></CompanyProtectedRoute>
+    path: "/companies",
+    element: (
+      <CompanyProtectedRoute>
+        <Companies />
+      </CompanyProtectedRoute>
+    ),
   },
   {
-    path:'/companies/create',
-    element:<CompanyProtectedRoute><CompanyCreate/></CompanyProtectedRoute>
+    path: "/companies/create",
+    element: (
+      <CompanyProtectedRoute>
+        <CompanyCreate />
+      </CompanyProtectedRoute>
+    ),
   },
   {
-    path:'/company/:id',
-    element:<CompanyProtectedRoute><CompanySetup/></CompanyProtectedRoute>
+    path: "/company/:id",
+    element: (
+      <CompanyProtectedRoute>
+        <CompanySetup />
+      </CompanyProtectedRoute>
+    ),
   },
   {
-    path:'/company/jobs',
-    element:<CompanyProtectedRoute><CompanyJobs/></CompanyProtectedRoute>
+    path: "/company/jobs",
+    element: (
+      <CompanyProtectedRoute>
+        <CompanyJobs />
+      </CompanyProtectedRoute>
+    ),
   },
   {
-    path:'/companies/job/create',
-    element:<CompanyProtectedRoute><PostJob/></CompanyProtectedRoute>
+    path: "/companies/job/create",
+    element: (
+      <CompanyProtectedRoute>
+        <PostJob />
+      </CompanyProtectedRoute>
+    ),
   },
   {
-    path:'/company/jobs/:id/applicants',
-    element:<CompanyProtectedRoute><Applicants/></CompanyProtectedRoute>
-  }
-])
-
+    path: "/company/jobs/:id/edit",
+    element: (
+      <CompanyProtectedRoute>
+        <PostJob />
+      </CompanyProtectedRoute>
+    ), 
+  },
+  {
+    path: "/company/jobs/:id/applicants",
+    element: (
+      <CompanyProtectedRoute>
+        <Applicants />
+      </CompanyProtectedRoute>
+    ),
+  },
+]);
 
 function App() {
-
   return (
     <>
-      <RouterProvider router={appRouter}/>
+      <RouterProvider router={appRouter} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
